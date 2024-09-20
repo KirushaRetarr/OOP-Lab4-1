29ebe91a1cc688582597facb475f08f1586cf6b6
Kirill Durnin <kirill2308@mail.ru>
2024-09-20 07:30:11 +0500

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Lab4_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox1.Text);
+            int[] genMass = new int[n];
+            Random randValue = new Random();
+
+            for (int i = 0; i < genMass.Length; i++)
+            {
+                genMass[i] = randValue.Next(1, 50);
+            }
+            
+            textBox4.Text = string.Join(" ", genMass);
+
+            List<int> evenMass = new List<int> { };
+            List<int> forSumMass = new List<int> { };
+
+            for (int i = 0; i < genMass.Length; i++) {
+                if (i % 2 == 0)
+                {
+                    evenMass.Add(genMass[i]);
+                }
+            }
+
+            for (int i = 1; i < genMass.Length - 1; i++)
+            {
+                forSumMass.Add(genMass[i]);
+            }
+
+            int multiResult = 1;
+            int sumResult = 0;
+
+            foreach (int num in evenMass)
+            {
+                multiResult *= num;
+            }
+
+            foreach (int num in forSumMass)
+            {
+                sumResult += num;
+            }
+
+            textBox2.Text = multiResult.ToString();
+            textBox3.Text = sumResult.ToString();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("OOP-Lab4-1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OOP-Lab4-1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("293142e3-ccc0-43b5-a81e-139a40c17685")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
